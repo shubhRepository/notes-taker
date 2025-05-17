b532318758828cdfbca2242189d072600eb8ce90
Shubham Maheshwari <academicShubh@gmail.com>
2025-05-17 09:43:07 +0530

    create controller to save notes

---
+++ src/main/java/com/example/notestaker/controller/GlobalController.java
@@ -0,0 +1,19 @@
+package com.example.notestaker.controller;
+
+//import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class GlobalController {
+
+    @PostMapping("/save-note")
+    public ResponseEntity<String> saveNote(@RequestBody String notesContent) {
+        System.out.println(notesContent);
+        return ResponseEntity.ok("Notes saved: " + notesContent);
+    }
+}
