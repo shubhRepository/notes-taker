28a889c1dbb9b0b730eed21f6edec0fcf5a3d574
Shubham Maheshwari <academicShubh@gmail.com>
2025-05-17 10:01:57 +0530

    parse the markdown to html

--- src/main/java/com/example/notestaker/controller/GlobalController.java
+++ src/main/java/com/example/notestaker/controller/GlobalController.java
@@ -1,6 +1,7 @@
 package com.example.notestaker.controller;
 
 //import org.springframework.web.bind.annotation.GetMapping;
+import com.example.notestaker.service.SaveNote;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,7 +14,7 @@ public class GlobalController {
 
     @PostMapping("/save-note")
     public ResponseEntity<String> saveNote(@RequestBody String notesContent) {
-        System.out.println(notesContent);
+        SaveNote.save(notesContent);
         return ResponseEntity.ok("Notes saved: " + notesContent);
     }
 }
